acb4afc9301ad43d0bb9a56f60c4799c517dd2c9
It's meee! Phat Marc <phfatm@gmail.com>
2020-05-06 15:33:31 +0200

    host url to database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-cylindrical-01104',
     user : 'postgres',
     password : 'post',
     database : 'facedb'
