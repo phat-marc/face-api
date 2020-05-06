a92ded0798b184db72f0b10b9016ef283352418b
It's meee! Phat Marc <phfatm@gmail.com>
2020-05-06 16:34:32 +0200

    icrymyselftosleep

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ const image = require('./controllers/image');
 
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true
-  }
-});
-
 // const db = knex({
 //   client: 'pg',
 //   connection: {
-//     host : '127.0.0.1',
-//     user : 'postgres',
-//     password : 'post',
-//     database : 'facedb'
+//     connectionString : process.env.DATABASE_URL,
+//     ssl: true
 //   }
 // });
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : ''
+  }
+});
 
 const app = express();
 app.use(bodyParser.json());
